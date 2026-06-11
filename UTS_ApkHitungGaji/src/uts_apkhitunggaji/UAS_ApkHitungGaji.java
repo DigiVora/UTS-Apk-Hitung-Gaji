2218d7be9d254518f1de5fad1012ff57ab55df47
achmad_khusnul_yakin <digivoraofficial81@gmail.com>
2026-06-11 17:57:56 +0700

    Upload Project UTS Apk Hitung Gaji

---
+++ UTS_ApkHitungGaji/src/uts_apkhitunggaji/UAS_ApkHitungGaji.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts_apkhitunggaji;
+
+/**
+ *
+ * @author achmad_khusnul_yakin
+ */
+public class UAS_ApkHitungGaji {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
